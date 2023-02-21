87d3323b51e3db2749fd8f43c48386f46041bcd3
sattaramaan <101286725+sattaramaan@users.noreply.github.com>
2023-02-21 17:33:57 +0530

    Add files via upload

---
+++ s2.js
@@ -0,0 +1,22 @@
+// Todo: Make M+ M- and MC functional
+let string = "";
+let vale;
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {
+      vale = parseInt(string);
+      vale = vale * 3.28084;
+      document.querySelector('input').value = vale;
+    }
+    else if (e.target.innerHTML == 'C') {
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else {
+      console.log(e.target)
+      string = string + e.target.innerHTML;
+      document.querySelector('input').value = string;
+    }
+  })
+})
